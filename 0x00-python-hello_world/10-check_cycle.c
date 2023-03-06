ce1b72d46dfa0eb4cc40bdb01f82510e71ee5de6
SKGakiria <swgakiria@gmail.com>
2023-03-06 12:29:13 -0800

    The beginning of python - updated

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,24 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * check_cycle - function that finds a loop in a linked list
+ * based on Floyd’s cycle-finding algorithm
+ * @list: pointer to the struct listint_t
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *slow = list, *fast = list;
 
-	if (!list)
-		return (0);
-
-	while (slow && fast && fast->next)
+	while (fast != NULL)
 	{
 		slow = slow->next;
+		if (fast->next == NULL)
+			break;
 		fast = fast->next->next;
 		if (slow == fast)
 			return (1);
 	}
-
 	return (0);
 }
