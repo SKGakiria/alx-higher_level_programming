c8d76731b795743e32c13b4b8daef17578a53577
SKGakiria <swgakiria@gmail.com>
2023-03-09 12:21:11 -0800

    02 Import & modules - amends

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,15 +5,23 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div 
     import sys
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+    argc = len(sys.argv)
+    result = 0
+    if argc != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    if sys.argv[2] != '+' and sys.argv[2] != '-' and sys.argv[2] != '*' and\
+       sys.argv[2] != '/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    if sys.argv[2] == '+':
+        result = add(int(sys.argv[1]), int(sys.argv[3]))
+    elif sys.argv[2] == '-':
+        result = sub(int(sys.argv[1]), int(sys.argv[3]))
+    elif sys.argv[2] == '*':
+        result = mul(int(sys.argv[1]), int(sys.argv[3]))
+    elif sys.argv[2] == '/':
+        result = div(int(sys.argv[1]), int(sys.argv[3]))
+    print('{:d} {} {:d} = {:d}'.format(int(sys.argv[1]), sys.argv[2],
+                                       int(sys.argv[3]), result))
