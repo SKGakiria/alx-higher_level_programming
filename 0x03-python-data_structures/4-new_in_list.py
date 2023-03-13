80f6e168b19eb41e7c688ef6d71647fd01a99844
SKGakiria <swgakiria@gmail.com>
2023-03-13 11:02:09 -0700

    Data structures: lists and tuples - updated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """Replaces an element in a list at a specific position without modifying the original list."""
+    """Replaces an element in a list at a specific position."""
     copy = []
     for num in my_list:
         copy.append(num)
