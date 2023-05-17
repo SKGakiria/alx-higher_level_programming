0a41e8e0675531488993dc44332a3f7e57f65d5d
SKGakiria <swgakiria@gmail.com>
2023-05-17 15:20:23 -0700

    Updated Task 3 to 16

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Script creating the table force_name on the MySQL server
-CREATE TABLE IF NOT EXISTS db_name.force_name (
-id INT,
-name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS `force_name` (
+`id` INT,
+`name` VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
 -- Script creating the table id_not_null on the MySQL server
-CREATE TABLE IF NOT EXISTS db_name.id_not_null (
-id INT DEFAULT 1,
-name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+`id` INT DEFAULT 1,
+`name` VARCHAR(256));
