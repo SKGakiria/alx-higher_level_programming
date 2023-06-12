24db065161b11bfae0947186f04fd0aef44b0134
SKGakiria <swgakiria@gmail.com>
2023-06-12 14:57:19 -0700

    Updated Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,16 +2,16 @@
 // script that prints a square
 
 const size = parseInt(process.argv[2]);
-let column = '';
+let row = '';
 
 if (isNaN(size)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    column += "X";
+    row += 'X';
   }
 
   for (let j = 0; j < size; j++) {
-    console.log(column);
+    console.log(row);
   }
 }
