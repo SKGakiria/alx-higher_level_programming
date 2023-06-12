4b230b97aea4d9deff272e9d8259861f262f28e6
SKGakiria <swgakiria@gmail.com>
2023-06-12 14:49:48 -0700

    Updated Task 4 and 5

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,4 +3,4 @@
 
 const myArg1 = process.argv[2];
 const myArg2 = process.argv[3];
-console.log(myArg1 + ' is ' + myArg2); 
+console.log(`${myArg1} is ${myArg2}`);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,9 @@
 // script that prints, if the first argument can be converted to an integer
 
 const arg = process.argv[2];
-if (isNaN(arg)) {
-  console.log(`Not a number`);
+const num = Number(arg);
+if (isNaN(num)) {
+  console.log('Not a number');
 } else {
   console.log('My number:', parseInt(arg));
 }
