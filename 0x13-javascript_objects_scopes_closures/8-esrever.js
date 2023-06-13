d99ee0828f4aee4d2251571cb90e86465311e74d
SKGakiria <swgakiria@gmail.com>
2023-06-13 13:18:31 -0700

    Updated Task 1 and 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -5,7 +5,7 @@ exports.esrever = function (list) {
   const rlistCopy = [];
   let i = 0;
   for (i = list.length - 1; i >= 0; i--) {
-   rlistCopy.push(list[i]);
+    rlistCopy.push(list[i]);
   }
-  return rlistCopy;
+  return (rlistCopy);
 };
