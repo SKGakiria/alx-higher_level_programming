c9d2fc8950531ce4af79d6afdd84c29e691e893c
SKGakiria <swgakiria@gmail.com>
2023-06-18 12:36:38 -0700

    Updated README.md file and Task 15 to 17

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,4 +11,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)i
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
