3cb359bfae746c09844a645a1a8d2132f0d97cfe
SKGakiria <swgakiria@gmail.com>
2023-07-26 13:19:28 +0000

    Updated files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script reads and prints the content of a file
 
-const fs = require('fs')
+const fs = require('fs');
 const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf-8', (error, f_content) => {
